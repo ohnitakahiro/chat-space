7e51ca15f437b509d6cd88aff6dfff705dbce2d2
ohnitakahiro <ohnitakahiro@gmail.com>
2020-04-08 11:55:47 +0900

    更新

--- Gemfile
+++ Gemfile
@@ -38,6 +38,11 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails', '~> 3.5'
+  gem 'rails-controller-testing'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'capybara'
 end
 
 group :development do
@@ -48,6 +53,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'ログインしている場合' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it '@messageに正しい値が入っていること' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it '@groupに正しい値が入っていること' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'index.html.erb に遷移すること do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'ログインしていない場合' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
+describe '#create' do
+  let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+  context 'ログインしている場合' do
+    before do
+      login user
+    end
+
+    context '保存に成功した場合' do
+      subject {
+        post :create,
+        params: params
+      }
+
+      it 'メッセージを保存すること' do
+        expect{ subject }.to change(Message, :count).by(1)
+      end
+
+      it 'group_messages_pathへリダイレクトすること' do
+        subject
+        expect(response).to redirect_to(group_messages_path(group))
+      end
+    end
+
+    context '保存に失敗した場合' do
+      let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+      subject {
+        post :create,
+        params: invalid_params
+      }
+
+      it 'メッセージを保存しないこと' do
+        expect{ subject }.not_to change(Message, :count)
+      end
+
+      it 'index.html.erbに遷移すること' do
+        subject
+        expect(response).to render_template :index
+      end
+    end
+  end
